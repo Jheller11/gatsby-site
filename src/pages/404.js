bdfc396be436983c96e32643d0f090c119398d46
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-10-22 17:01:31 -0400

    initial commit - create new gatsby project

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const NotFoundPage = () => (
+  <Layout>
+    <h1>NOT FOUND</h1>
+    <p>This page does not exist. Please go back and try again.</p>
+  </Layout>
+)
+
+export default NotFoundPage
