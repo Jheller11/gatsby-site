ff66a51511be77b8cc5829b3bea1e679fd44e865
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-10-25 20:24:18 -0400

    start adding resources

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -2,11 +2,35 @@ import React from 'react'
 import Layout from '../components/layout'
 
 const Resources = () => {
-  const resources = []
+  const array = [
+    {
+      name: 'HTML Validator',
+      link: 'https://validator.w3.org/#validate_by_input',
+      description: 'HTML Validator from W3',
+    },
+    {
+      name: 'CSS Validator',
+      link: 'https://jigsaw.w3.org/css-validator/#validate_by_input',
+      description: 'CSS Validator from W3',
+    },
+    {
+      name: 'JS Validator',
+      link: 'https://codebeautify.org/jsvalidate',
+      description: 'JS Validator (Code Beautify)',
+    },
+  ]
+  const resources = array.map(item => {
+    return (
+      <li>
+        <a href={item.link}>{item.name}</a>
+        <p>{item.description}</p>
+      </li>
+    )
+  })
   return (
     <Layout>
       <p className="page-description">Useful links, tools, etc.</p>
-      <div>{resources}</div>
+      <ul>{resources}</ul>
     </Layout>
   )
 }
