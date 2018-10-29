4a1be4e7f5d6adaf380c1ee6c8af90266e376c54
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-10-29 15:34:02 -0400

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
 const Footer = () => {
-  return <footer>my footer</footer>
+  return (
+    <footer className="flex-2-columns">
+      <div>Created by Jeffrey Heller</div>
+      <div>
+        <a
+          style={{ color: 'red', textDecoration: 'none' }}
+          href="mailto:heller.jeffrey@gmail.com"
+        >
+          heller.jeffrey@gmail.com
+        </a>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
